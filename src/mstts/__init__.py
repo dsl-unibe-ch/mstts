cff9eabe2effb602dc90d69f2408e13273bcd9e7
Mykhailo Vladymyrov @DSL <neworldemancer@gmail.com>
2026-06-29 13:49:24 +0200

    update importables

--- src/mstts/__init__.py
+++ src/mstts/__init__.py
@@ -3,3 +3,7 @@ Main module for mstts package.
 
 MSTTS - Multi-Class Stratified Train Test Spliter
 """
+
+from mstts.mstts import get_row_selection, train_test_split
+
+__all__ = ["get_row_selection", "train_test_split"]
